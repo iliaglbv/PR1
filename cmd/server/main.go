68a997d11b4fc09d3057ae34d2ce559b8a1ceefa
iliaglbv <iliago5@mail.ru>
2025-12-09 15:32:57 +0300

    PZ1: minimal HTTP server + uuid

---
+++ cmd/server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"github.com/google/uuid"
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type health struct {
+	Status   string `json:"status"`
+	Time string `json:"time"`
+}
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(user{
+			ID:   uuid.NewString(),
+			Name: "Gopher",
+		})
+	})
+	//эндпоинт health с JSON содержащим status и time
+		mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(health{
+			Status:	"ok",
+			Time: time.Now().Format(time.RFC3339),
+		})
+	})
+
+	addr := ":" + port
+	log.Printf("Starting on %s ...", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
